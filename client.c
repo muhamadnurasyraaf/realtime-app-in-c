b39eacd5d4d16678e6df79a614c1a9786ad6ac02
muhamadnurasyraaf <masyraaf14@gmail.com>
2024-08-16 17:10:28 +0800

    test

---
+++ client.c
@@ -0,0 +1,51 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+
+#define PORT 8070
+#define BUFFER_SIZE 1024
+
+int main() {
+    int sock = 0;
+    struct sockaddr_in serv_addr;
+    char buffer[BUFFER_SIZE] = {0};
+    const char *hello = "Hello from client";
+
+    // Create socket file descriptor
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("Socket creation error\n");
+        return -1;
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    // Convert IPv4 and IPv6 addresses from text to binary form
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+        printf("Invalid address/ Address not supported\n");
+        return -1;
+    }
+
+    // Connect to the server
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        printf("Connection Failed\n");
+        return -1;
+    }
+
+    // Send a message to the server
+    send(sock, hello, strlen(hello), 0);
+    printf("Hello message sent\n");
+
+    // Receive a message from the server
+    int valread = read(sock, buffer, BUFFER_SIZE);
+    printf("Received from server: %s\n", buffer);
+
+    // Close the socket
+    close(sock);
+    return 0;
+}
+
--- server.c
+++ server.c
@@ -17,22 +17,23 @@ int main(){
     int addrlen = sizeof(address);
 
     char buffer[BUFFER_SIZE] = {0};
-
+    
     const char *welcome_message = "Hello from server";
+
+    //create socket file descriptor
     if((server_fd = socket(AF_INET,SOCK_STREAM,0)) < 0 ){
         perror("Socket creation error\n");
         return -1;
     }
 
-
+    //configure server address
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
 
-
     printf("%d \n",server_fd);
 
-
+    //bind socket to the port
     if(bind(server_fd,(struct sockaddr *)&address,sizeof(address)) < 0){
         perror("bind failed");
         close(server_fd);
@@ -41,8 +42,31 @@ int main(){
 
     if(listen(server_fd,3) < 0){
         perror("failed bang");
-        return -1;
+        exit(EXIT_FAILURE);
     }
+
+     printf("Server listening on port %d...\n", PORT);
+
+    // Accept a connection
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
+        perror("accept failed");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    // Send a welcome message to the client
+    send(new_socket, welcome_message, strlen(welcome_message), 0);
+    printf("Welcome message sent\n");
+
+    // Receive a message from the client
+    int valread = read(new_socket, buffer, BUFFER_SIZE);
+    printf("Received from client: %s\n", buffer);
+
+    // Close the connection
+    close(new_socket);
+    close(server_fd);
+    return 0;
+
 
     return 0;
 }
